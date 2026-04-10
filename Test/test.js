8591fb55a1c026d9ccc831ac655f435dea7402c1
techuv4-dotcom <techuv4@gmail.com>
2026-04-10 07:07:58 +0000

    hg

--- Test/test.js
+++ Test/test.js
@@ -1,3 +1,44 @@
-console.log("hello world")
+//console.log("hello world")
 const acountId = 123456;
-console.log(acountId);
+//console.log(acountId);
+
+let gmail = "hello@gmail.com"
+let password ="123456";
+
+//console.log([acountId,gmail,password]);
+/* 
+data typs
+1. string 
+2. number
+3. bigint
+4. null
+5. undifind
+*/
+let number = 123
+let string = "string"
+let boolean = true
+let nuull = null
+let undifineed
+
+// console.log(typeof(number));
+// console.log(typeof(string));
+// console.log(typeof(nuull));
+// console.log(typeof(undifineed));
+
+/*
+change data types 
+*/
+
+let stringToNum = Number(string)
+let numToString = String(number)
+let booleanToString = String(boolean)
+
+// console.log(stringToNum)
+// console.log(numToString)
+// console.log(typeof(boolean))
+// console.log(booleanToString,typeof(booleanToString))
+console.log(1 + 2 + "1" + 2 + 3)
+
+let a = 1
+let b = ++a
+console.log(b)
