c0f51bc263dd546cacf92a86d67b12681a1be8ea
techuv4-dotcom <techuv4@gmail.com>
2026-04-11 07:19:29 +0000

    datatypes

---
+++ Test/dataTypes.js
@@ -0,0 +1,46 @@
+/*there is two types of data types
+1.premitive data types
+string
+number
+boolean
+null
+undifined
+symbol
+
+2. nonprimitev
+
+array
+function
+object
+*/
+//+++++++++++++++++++++++++++++++++++++++  example  +++++++++++++++++++++++++++++++++++
+
+let str = "hellow world"
+let num = 123
+let bool = true
+let nul  = null
+let undif
+let sym = Symbol
+
+// console.log(typeof str);
+// console.log(typeof num);
+// console.log(typeof bool);
+// console.log(typeof nul);
+// console.log(typeof undif);
+// console.log(typeof sym);
+
+let array = [1,2,3,4]
+
+function fun(){
+    console.log("this is a function");   
+}
+
+let obj = {
+     name: "yogesh",
+     age: 26
+}
+
+console.log(array)
+fun()
+console.log(obj.name,obj.age);
+
